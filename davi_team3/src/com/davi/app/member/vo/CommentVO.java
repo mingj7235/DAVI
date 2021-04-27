f7281663c2d8f046288995696205a31639db3ec7
김민재 <joshuara7235@gmail.com>
2021-04-27 11:22:10 +0900

    Debug for payInput.jsp and davi comment added

---
+++ davi_team3/src/com/davi/app/member/vo/CommentVO.java
@@ -0,0 +1,39 @@
+package com.davi.app.member.vo;
+
+public class CommentVO {
+	
+	private int commentNum;
+	private String column1;
+	private String column2;
+	private String column3;
+	
+	public CommentVO() {;}
+	
+	public int getCommentNum() {
+		return commentNum;
+	}
+	public void setCommentNum(int commentNum) {
+		this.commentNum = commentNum;
+	}
+	public String getColumn1() {
+		return column1;
+	}
+	public void setColumn1(String column1) {
+		this.column1 = column1;
+	}
+	public String getColumn2() {
+		return column2;
+	}
+	public void setColumn2(String column2) {
+		this.column2 = column2;
+	}
+	public String getColumn3() {
+		return column3;
+	}
+	public void setColumn3(String column3) {
+		this.column3 = column3;
+	}
+	
+}
+
+
